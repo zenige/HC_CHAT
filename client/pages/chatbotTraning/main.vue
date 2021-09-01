05378c8b9a27213d5f1a78ed1ca51fbb3df1dc0c
salongbas1 <thanapon_bas2000@hotmail.com>
2021-09-01 23:00:59 +0700

    add  newWord example

--- client/pages/chatbotTraning/main.vue
+++ client/pages/chatbotTraning/main.vue
@@ -7,10 +7,22 @@
       <div class="container fitscreen pt-3 pt-md-3 plr_15p">
         <div class="row">
           <div class="col-md-12 pb_me-4">
-            <div class="row d-flex justify-content-center">Test</div>
+            <div class="row d-flex justify-content-center">
+              <div class="ml-5 mt-5">
+                <b-card no-body>
+                  <b-tabs card>
+                    <b-tab title="Tab 1" active>
+                      <newWord/>
+                    </b-tab>
+                    <b-tab title="Tab 2">
+                      <b-card-text>Tab contents 2</b-card-text>
+                    </b-tab>
+                  </b-tabs>
+                </b-card>
+              </div>
+            </div>
           </div>
         </div>
-        <nuxt-child></nuxt-child>
       </div>
     </div>
   </div>
@@ -19,6 +31,9 @@
 <script>
 export default {
   layout: 'noHeaderLayout',
+  components: {
+    newWord: () => import('~/components/chatbotTraning/newWord.vue'),
+  }
 }
 </script>
 
