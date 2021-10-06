4d54c4fc5bf929a7b63adca6ef1fd2495199f0eb
Zenige <zenigecarefully@gmail.com>
2021-10-06 17:33:10 +0700

    remove oldname from basemodel

--- api/router/group.py
+++ api/router/group.py
@@ -7,6 +7,8 @@ import datetime
 from typing import Any, Dict, AnyStr, List, Union
 from model.Logic import Logic
 from firebase_admin import firestore
+from model.Feature import updateFeature
+import ast
 router = APIRouter()
 
 
@@ -28,3 +30,34 @@ def streamToList(docs):
         fakeDict = doc.to_dict()
         userGroup.append(fakeDict)
     return userGroup,count
+
+@router.patch("/")
+async def getUsers(body:updateFeature):
+    body = dict(body)
+    # docs = db.collection(u'feature').document(body['id'])
+    # docs.update({u'Name': body['Name']})
+    # db.collection(u'feature').document(body['Name']).set(body)
+    
+    new_key = body['Name']
+    old_key = body['old_Name']
+    docs = db.collection("logics").document(old_key).get()
+    db.collection(u'logics').document(old_key).delete()
+    # users = []
+
+
+    features = []
+    data =  docs.to_dict()
+    dataStr = data['data']
+    group = ast.literal_eval(dataStr)
+
+  
+    group['group'] = new_key
+
+    features.append(group)
+    for feature in features:
+        featureStr = str(feature)
+        # print(featureStr)
+        # print(feature['group'])
+        # db.collection(u'logics').document(feature['group']).delete()
+        db.collection(u'logics').document(feature['group']).set({"data":featureStr})
+    return "update done"
