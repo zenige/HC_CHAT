ec6d3ee5e2fca39d4b3e9897ec1a0ea148b653ee
Bawornsak Luachan <59905945+Bawornsak@users.noreply.github.com>
2021-10-12 11:59:28 +0700

    commit

--- model/router/trainModel.py
+++ model/router/trainModel.py
@@ -1,9 +1,12 @@
+from typing import Any, Dict, AnyStr, List, Union, Set
+from pydantic import BaseModel, HttpUrl
+import json
 from firebase_admin import firestore
 import pickle
-from fastapi import APIRouter,Request
+from fastapi import APIRouter, Request
 from Project import db
-from Project.create_model import cleanData, create_model,TrainModelSVM,checkOr,checkOrPredict
-from Project.test_mode import testModelRF,readCSV,cleanTestData, testModelRF, testModelSVM
+from Project.create_model import cleanData, create_model, TrainModelSVM, checkOr, checkOrPredict
+from Project.test_mode import testModelRF, readCSV, cleanTestData, testModelRF, testModelSVM
 import ttg
 import pandas as pd
 from sklearn.datasets import load_iris
@@ -14,11 +17,12 @@ import ast
 from sklearn.ensemble import RandomForestClassifier
 warnings.filterwarnings("ignore")
 router = APIRouter()
-from pydantic import BaseModel,HttpUrl
-from typing import Any, Dict, AnyStr, List, Union ,Set 
+
+
 class user(BaseModel):
 
     userId: str
+
 
 class group(BaseModel):
     group: str
@@ -31,7 +35,6 @@ JSONArrayCREATE = List
 JSONStructureDELETE = Union[List, JSONObject]
 
 
-import json
 @router.get("/")
 async def test():
     docs_ref = db.collection(u'logics')
@@ -49,16 +52,16 @@ async def test():
 
 @router.get("/createModel")
 async def createModel():
-    datas = [{"group": "group0", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "ANY", "tired": "ANY", "close": "false", "Travel": "false"}, 
-            {"group": "group1", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "false", "tired": "ANY", "close": "true", "Travel": "true"}, {
-            "group": "group1p2", "AGE": "ANY", 'disease': "ANY", "cough": "false", "Fever": "true", "tired": "false", "close": "true", "Travel": "true"}, {
-            "group": "group2", "AGE": 4, 'disease': "false", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]}, {
-            "group": "group3", "AGE": 5, 'disease': "false", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]
-    },{
-            "group": "group3", "AGE": "ANY", 'disease': "true", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]
+    datas = [{"group": "group0", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "ANY", "tired": "ANY", "close": "false", "Travel": "false"},
+             {"group": "group1", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "false", "tired": "ANY", "close": "true", "Travel": "true"}, {
+        "group": "group1p2", "AGE": "ANY", 'disease': "ANY", "cough": "false", "Fever": "true", "tired": "false", "close": "true", "Travel": "true"}, {
+        "group": "group2", "AGE": 4, 'disease': "false", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]}, {
+        "group": "group3", "AGE": 5, 'disease': "false", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]
+    }, {
+        "group": "group3", "AGE": "ANY", 'disease': "true", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]
     },
-    {
-             "group": "group4", "AGE": "ANY", 'disease': "ANY", "Fever": "true", "close": "ANY", "Travel": "true","cough":"true","tired":"true"
+        {
+        "group": "group4", "AGE": "ANY", 'disease': "ANY", "Fever": "true", "close": "ANY", "Travel": "true", "cough": "true", "tired": "true"
     }
     ]
     newDF = await create_model(datas)
@@ -72,7 +75,7 @@ async def createModel():
 
 @router.get("/createModelRF")
 async def createModelRF():
-    # datas = [{"group": "group0", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "ANY", "tired": "ANY", "close": "false", "Travel": "false"}, 
+    # datas = [{"group": "group0", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "ANY", "tired": "ANY", "close": "false", "Travel": "false"},
     #         {"group": "group1", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "false", "tired": "ANY", "close": "true", "Travel": "true"}, {
     #         "group": "group1p2", "AGE": "ANY", 'disease': "ANY", "cough": "false", "Fever": "true", "tired": "false", "close": "true", "Travel": "true"}, {
     #         "group": "group2", "AGE": 4, 'disease': "false", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]}, {
@@ -88,7 +91,7 @@ async def createModelRF():
     datas = []
     data = {}
     for doc in docs:
-        data =  doc.to_dict()
+        data = doc.to_dict()
         dataStr = data['data']
         dataDict = ast.literal_eval(dataStr)
         datas.append(dataDict)
@@ -99,16 +102,18 @@ async def createModelRF():
     print(groupName)
     newDF = await create_model(datas)
 
-    newDF = cleanData(newDF,groupName)
+    newDF = cleanData(newDF, groupName)
 
     TrainModelRF(newDF)
 
     return "newDF"
 
+
 @router.get("/test")
 async def test():
     checkOr()
     return "newDF"
+
 
 def createTruthTable(datas):
     newDF = pd.DataFrame()
@@ -156,13 +161,14 @@ async def predict():
     if doc.exists:
         userInfo = doc.to_dict()
         userInfo['symptom']['AGE'] = 4
-        df = pd.DataFrame([userInfo['symptom']], columns=userInfo['symptom'].keys())
+        df = pd.DataFrame([userInfo['symptom']],
+                          columns=userInfo['symptom'].keys())
 
         df = await checkOrPredict(df)
         df = df.reindex(sorted(df.columns), axis=1)
         df = cleanData(df)
         print(df)
-        result = loaded_model.predict([[8.0,1,1,1,0,0]])
+        result = loaded_model.predict([[8.0, 1, 1, 1, 0, 0]])
         print(result[0])
 
     else:
@@ -177,8 +183,9 @@ async def predict():
     else:
         return "something went wrong"
 
+
 @router.post("/predictRF")
-async def predict(user:user):
+async def predict(user: user):
     print(user)
     # userId = "Ueb59687406ee1813431033235e2b83ec"
     loaded_model = pickle.load(open("RF_model.sav", 'rb'))
@@ -188,27 +195,28 @@ async def predict(user:user):
     if doc.exists:
         userInfo = doc.to_dict()
         age = int(userInfo['symptom']['AGE'])
-        if age <= 22 and age >= 0 :
+        if age < 22 and age >= 0:
             agegroup = 2
-        elif age <= 30 :
+        elif age <= 30:
             agegroup = 3
-        elif age <= 40 :
+        elif age <= 40:
             agegroup = 4
-        elif age <= 50 :
+        elif age <= 50:
             agegroup = 5
-        elif age <= 60 :
+        elif age <= 60:
             agegroup = 6
         elif age > 60:
             agegroup = 7
         userInfo['symptom']['AGE'] = agegroup
-        df = pd.DataFrame([userInfo['symptom']], columns=userInfo['symptom'].keys())
+        df = pd.DataFrame([userInfo['symptom']],
+                          columns=userInfo['symptom'].keys())
         print(df)
         df = await checkOrPredict(df)
         print(df)
         df = df.reindex(sorted(df.columns), axis=1)
         groupName = None
         print(df)
-        df = cleanData(df,groupName)
+        df = cleanData(df, groupName)
         print(df)
         result = loaded_model.predict(df)
         docs = db.collection("logics").stream()
@@ -216,7 +224,7 @@ async def predict(user:user):
         data = {}
         answer = ''
         for doc in docs:
-            data =  doc.to_dict()
+            data = doc.to_dict()
             dataStr = data['data']
             dataDict = ast.literal_eval(dataStr)
             datas.append(dataDict)
@@ -249,14 +257,13 @@ async def predict(user:user):
         return "something went wrong"
 
 
-
-
 @router.get("/testmodelSVM")
 async def testModelRoute():
     df = readCSV()
     df = cleanTestData(df)
     testModelSVM(df)
     return "newDF"
+
 
 @router.get("/testmodelRF")
 async def testModelRoute():
@@ -266,39 +273,36 @@ async def testModelRoute():
     return "newDF"
 
 
-
-
-
-
 @router.post('/addlogic')
-async def testna( request: Request):
+async def testna(request: Request):
     # data = await request.json()
 
-    data = [{"group": "group0", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "ANY", "tired": "ANY", "close": "false", "Travel": "false"}, 
+    data = [{"group": "group0", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "ANY", "tired": "ANY", "close": "false", "Travel": "false"},
             {"group": "group1", "AGE": "ANY", 'disease': "ANY", "cough": "ANY", "Fever": "false", "tired": "ANY", "close": "true", "Travel": "true"}, {
             "group": "group1p2", "AGE": "ANY", 'disease': "ANY", "cough": "false", "Fever": "true", "tired": "false", "close": "true", "Travel": "true"}, {
             "group": "group2", "AGE": 4, 'disease': "false", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]}, {
             "group": "group3", "AGE": 5, 'disease': "false", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]
-    },{
+            }, {
             "group": "group3p2", "AGE": "ANY", 'disease': "true", "Fever": "true", "close": "true", "Travel": "true", "Relation": [["cough", "tired"]]
-    },
-    {
-             "group": "group4", "AGE": "ANY", 'disease': "ANY", "Fever": "true", "close": "ANY", "Travel": "true","cough":"true","tired":"true"
+            },
+            {
+        "group": "group4", "AGE": "ANY", 'disease': "ANY", "Fever": "true", "close": "ANY", "Travel": "true", "cough": "true", "tired": "true"
     }
     ]
-    for i in data :
+    for i in data:
         print(i)
         group = str(i)
-        db.collection("logics").document(i['group']).set({"data":group})
+        db.collection("logics").document(i['group']).set({"data": group})
     return "done"
 
+
 @router.post('/addlineLogic')
-async def testna( request: Request):
+async def testna(request: Request):
     data = await request.json()
     print(data)
 
-    for i in data :
+    for i in data:
         print(i)
         group = str(i)
-        db.collection("logics").document(i['group']).set({"data":group})
+        db.collection("logics").document(i['group']).set({"data": group})
     return "done"
