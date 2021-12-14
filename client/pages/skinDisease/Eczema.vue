0a77e44ab121831f53194d6adafebfd0aeda8233
zenige <zenigecarefully@gmail.com>
2021-12-14 17:42:47 +0700

    a

--- client/pages/skinDisease/Eczema.vue
+++ client/pages/skinDisease/Eczema.vue
@@ -761,44 +761,32 @@ export default {
       liff
         .sendMessages([
           {
-            type: 'text',
-            text: 'Hello, World!',
-          },
-          {
-  "type": "template",
-  "altText": "This is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "imageAspectRatio": "rectangle",
-      "imageSize": "cover",
-      "imageBackgroundColor": "#FFFFFF",
-      "title": "Menu",
-      "text": "Please select",
-      "defaultAction": {
-          "type": "uri",
-          "label": "View detail",
-          "uri": "http://example.com/page/123"
+  "type": "text", // ①
+  "text": "Select your favorite food category or send me your location!",
+  "quickReply": { // ②
+    "items": [
+      {
+        "type": "action", // ③
+        "imageUrl": "https://i.insider.com/60775d9042061500181758e5?width=1300&format=jpeg&auto=webp",
+        "action": {
+          "type": "message",
+          "label": "Sushi",
+          "text": "Sushi"
+        }
       },
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
+      {
+        "type": "action",
+        "imageUrl": "https://i.insider.com/60775d9042061500181758e5?width=1300&format=jpeg&auto=webp",
+        "action": {
+          "type": "message",
+          "label": "Tempura",
+          "text": "Tempura"
+        }
+      }
+    ]
   }
 }
+
         ])
         .then(() => {
           console.log('message sent')
